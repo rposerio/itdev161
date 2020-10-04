c8c5d23c84fd990fc313eea3ff4b6d68eeb13fff
rposerio <rposerio@icloud.com>
2020-10-04 07:33:45 -0700

    update activity

--- server.js
+++ server.js
@@ -1,36 +1,52 @@
 import express from 'express';
 import connectDatabase from './config/db';
+import { check, validationResult } from 'express-validator';
+import cors from 'cors';
 
-
-//initialize express application
+// Initialize express application
 const app = express();
 
-//connect database
+// Connect Database
 connectDatabase();
 
-//confiugure Middleware
-app.use(express.json({ extend: false }));
+// Configure Middleware
+app.use(express.json({ extended: false }));
+app.use(
+    cors({
+        origin: 'http://localhost:3000'
+    })
+);
 
-// API endpoints
-/** 
- * @route GET /
- * @desc Test Endpoint
-*/
-app.get('/', (req, res) =>
+// API end points
+/**
+ * @route GET/
+ * @desc Test endpoint
+ */
+app.get('/', (req,res) =>
     res.send('http get request sent to root api endpoint')
-    );
-
+);
 
 /**
  * @route POST api/users
  * @desc Register user
  */
-app.post('/api/users', (req,res) => {
-    console.log(req.body);
-    res.send(req.body);
-});
+app.post(
+    '/api/users',
+    [
+        check('name', 'Please enter your name').not().isEmpty(),
+        check('email', 'Please enter a valid email').isEmail(),
+        check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6})
+    ],
+    (req, res) =>{
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        } else {
+            return res.send(req.body);
+        }
+    }
+);
 
-
-
-//connection listener
-app.listen(3000, () => console.log('Express server running on port 3000'));
+// Connection listener
+const port = 5000;
+app.listen(port, () => console.log(`Express server running on port ${port}`));
