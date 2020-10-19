56665bb9c42dfe61d6a648468e43fd994547b7d6
rposerio <rposerio@icloud.com>
2020-10-19 03:39:12 -0500

    complete activity 4

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from 'express';
+import { check, validationResult } from 'express-validator';
 import connectDatabase from './config/db';
+import cors from 'cors';
 
 //initialize express application
 const app = express();
@@ -7,10 +9,39 @@ const app = express();
 // conmnect to database
 connectDatabase();
 
+app.use(express.json({ extend: false}));
+app.use(
+    cors({
+        origin: 'http:localhost:3000'
+    })
+);
+
 //API endpoints
 app.get('/', (req, res) => 
     res.send('http get request sent to root api endpoint')
     );
 
-//Connection listner
-app.listen(3000, () => console.log('Express server running on port 3000'));
+app.post(
+    '/api/users',
+    [
+        check('name', 'Please enter your name')
+        .not()
+        .empty(),
+        check('email', 'Please enter a valid email').isEmail(),
+        check(
+            'password', 'please enter a password with 6 or more characters'
+        ).isLength({ min: 6})
+    ],
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ error: errors.array() });
+        }else {
+            return res.send(req.body);
+        }
+    }
+);
+
+//Connection listener
+const port = 5000;
+app.listen(port, () => console.log(`Express server running on port ${port}`));
