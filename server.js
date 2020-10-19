e72bda4f2276f29704ee6409ec848fab4f6c646d
rposerio <rposerio@icloud.com>
2020-10-19 01:24:24 -0500

    Added validation

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import connectDatabase from './config/db';
+import {check, validationResult } from 'express-validator';
 
 //initialize express application
 const app = express();
@@ -25,10 +26,27 @@ app.get('/', (req, res) =>
  * @route POST api/users
  * @desc Register User
  */
-app.post('/api/users', (req,res) => {
-    console.log(req.body);
-    res.send(req.body);
-});
+app.post(
+    '/api/users',
+    [
+        check('name', 'Please enter your name')
+        .not()
+        .isEmpty(),
+        check('email', 'Please enter a valid email').isEmail(),
+        check(
+            'password', 
+            'please enter a password with 6 or more character'
+            ).isLength({ min: 6 })
+    ],
+     (req,res) => {
+         const errors = validationResult(req);
+         if (!errors.isEmpty()){
+             return res.status(422).json({ errors: errors.array() });
+         }else {
+             return res.send(req.body);
+         }
+        }
+    );
 
 //Connection listner
 app.listen(3000, () => console.log('Express server running on port 3000'));
