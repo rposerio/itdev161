47a949c24e34c5b05f491f5714675bb0fa1f2050
rposerio <rposerio@icloud.com>
2020-10-19 01:34:34 -0500

    Completed Activity 3

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import connectDatabase from './config/db';
-import {check, validationResult } from 'express-validator';
+import  { check, validationResult } from 'express-validator';
 
 //initialize express application
 const app = express();
@@ -35,18 +35,17 @@ app.post(
         check('email', 'Please enter a valid email').isEmail(),
         check(
             'password', 
-            'please enter a password with 6 or more character'
+            'please enter a password with 6 or more characters'
             ).isLength({ min: 6 })
     ],
      (req,res) => {
          const errors = validationResult(req);
          if (!errors.isEmpty()){
              return res.status(422).json({ errors: errors.array() });
-         }else {
+         } else {
              return res.send(req.body);
          }
-        }
-    );
-
+    }
+);
 //Connection listner
 app.listen(3000, () => console.log('Express server running on port 3000'));
